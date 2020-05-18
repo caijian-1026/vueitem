fa6d94ead0f7de0150247efa9104a332a3410925
caijian <caijian_1026@163.com>
2020-05-18 11:27:21 +0800

    douban第一次提交

--- douban/doubanserver/server.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var app = express();
-var routerMovie = require("./router/movie");
-    app.use("/movie",routerMovie);
-    app.use("/vuex",routerMovie);
-var routerUser = require("./router/user");
-    app.use("/user",routerUser);
-    app.listen(8787);
